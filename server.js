7eadfe42c6b5a51bcbd631e72a9af5ce4bf94c1e
Mihailova Tatiana <tatiana.mihailova@isa.utm.md>
2025-05-29 12:35:38 +0300

    swagger

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const plantRoutes = require('./routes/plantRoutes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger/swagger.json');
+
 
 dotenv.config();
 
@@ -12,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/plants', plantRoutes);
 app.use('/', require('./routes/authRoutes'));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
 
